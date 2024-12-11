65d2273bc4595cedc00ef0fa25a028fd92d9befe
Abel Martinez <abel_martinez7@hotmail.com>
2024-12-11 20:30:28 +0100

    chore: remove vercel package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercelServerless(),
+  output: "static",
 });
