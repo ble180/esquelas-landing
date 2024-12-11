d39fcb311dff76315236e2de7229445210511e65
Abel Martinez <abel_martinez7@hotmail.com>
2024-12-11 20:24:08 +0100

    feat: first version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
-
-import netlify from "@astrojs/netlify/functions";
+import { defineConfig } from "astro/config";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: vercelServerless(),
 });
